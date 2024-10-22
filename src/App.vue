3a78d4ca5a3cf58a5c913adbbd34aa448f20fa72
Gabriel Vieira <gabkv99@hotmail.com>
2024-10-22 17:47:56 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<script setup>
+  import { reactive } from 'vue';
+  import Formulario from './components/Formulario.vue';
+
+  const estado = reactive({
+    NumeroASerConvertido: 0,
+    filtro: 'pesoChileno',
+  })
+
+const pesoChilenoReal = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido * 0.00597973;
+}
+
+const dolarReal = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido * 5.692;
+}
+
+const euroReal = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido * 6.1491;
+}
+
+const wonReal = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido * 0.00412449 ;
+}
+
+const ieneReal = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido * 0.0376637;
+}
+
+const realPesoChileno = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido / 0.00597973;
+}
+
+const realDolar = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido / 5.692;
+}
+
+const realEuro = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido / 6.1491;
+}
+
+const realWon = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido / 0.00412449 ;
+}
+
+const realIene = ({ NumeroASerConvertido } = estado) => {
+  return NumeroASerConvertido / 0.0376637;
+}
+
+const resultado = () => {
+  const {filtro} = estado;
+
+    switch (filtro) {
+      case 'dolarReal':
+        return dolarReal();
+      case 'euroReal':
+        return euroReal();
+      case 'wonReal':
+        return wonReal();
+      case 'ieneReal':
+        return ieneReal();
+      case 'realPesoChileno':
+        return realPesoChileno();
+      case 'realDolar':
+        return realDolar();
+      case 'realEuro':
+        return realEuro();
+      case 'realWon':
+        return realWon();
+      case 'realIene':
+        return realIene();
+      default:
+        return pesoChilenoReal();    
+    }
+  }
+
+</script>
+
+<template>
+  <div class="container">
+    <header class="p-5 mb-4 mt-4 text-center">
+        <h1>Conversão de moedas:</h1>
+    </header>
+    <Formulario
+      :trocar-filtro="(event) => (estado.filtro = event.target.value)"
+      :numero1="(event) => (estado.NumeroASerConvertido = Number(event.target.value))"
+    />
+    <br>
+    <br>
+    <h1 class="text-center border border-dark rounded p-5">O resultado é: {{ resultado() }} </h1>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,48 @@
+<script setup>
+const props = defineProps(["numero1", "numero2", "trocarFiltro"]);
+</script>
+
+<template>
+    <form>
+        <div class="row">
+            <div class="col d-flex">
+            <input @keyup="props.numero1" required type="number" placeholder="Digite o número a ser convertido" class="form-control border-dark">
+            </div>
+            <div class="col-md-4 alteraCelular">
+                <select @change="props.trocarFiltro" class="alinhaTexto form-control border-dark">
+                    <option value="pesoChilenoReal">Peso Chileno para Real</option>
+                    <option value="dolarReal">Dólar para Real</option>
+                    <option value="euroReal">Euro para Real</option>]
+                    <option value="wonReal">Won para Real</option>
+                    <option value="ieneReal">Iene para Real</option>
+                    <option value="realPesoChileno">Real para Peso Chileno</option>
+                    <option value="realDolar">Real para Dólar</option>
+                    <option value="realEuro">Real para Euro</option>]
+                    <option value="realWon">Real para Won</option>
+                    <option value="realIene">Real para Iene</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
+
+<style scoped>
+form input {
+    margin-right: 16px;
+    text-align: center;
+}
+
+.alinhaTexto {
+    text-align: center;
+}
+
+@media only screen and (max-width: 767px) {
+    .alteraCelular {
+        margin-top: 16px;
+    }
+
+    form input {
+    margin-right: 3px;
+}
+}
+</style>
